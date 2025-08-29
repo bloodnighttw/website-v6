1439f64fef6553141e452215af26071d94b17f1a
bloodnighttw <bbeenn1227@gmail.com>
2025-08-29 19:32:04 +0800

    refactor: rename

--- packages/rpress/src/vite/index.ts
+++ packages/rpress/src/vite/index.ts
@@ -1,82 +1,10 @@
-import { type Plugin, type ResolvedConfig } from "vite";
+import { type Plugin } from "vite";
 import rsc from "@vitejs/plugin-rsc";
-import path, { normalize } from "path";
-import { pathToFileURL } from "url";
-import { HTML_POSTFIX, RSC_POSTFIX } from "../config";
-import { Readable } from "stream";
-import fs from "node:fs";
+import { rscSsgPlugin as ssg } from "./ssg";
 
 const PKG_NAME = "rpress";
 
-function rscSsgPlugin(): Plugin[] {
-  return [
-    {
-      name: "rsc-ssg",
-      config(_config, env) {
-        if (env.isPreview) {
-          return {
-            appType: "mpa",
-          };
-        }
-      },
-      buildApp: {
-        async handler(builder) {
-          await renderStatic(builder.config);
-        },
-      },
-    },
-  ];
-}
-
-async function renderStatic(config: ResolvedConfig) {
-  // import server entry
-  const entryPath = path.join(config.environments.rsc.build.outDir, "index.js");
-  const entry: typeof import("../entry/rsc") = await import(
-    pathToFileURL(entryPath).href
-  );
-
-  // entry provides a list of static paths
-  const allRouteModules = entry.allRouteModules;
-
-  const staticPaths = (
-    await Promise.all(
-      allRouteModules.map(async (module) => {
-        const generator = module.config.config.generator;
-        const matcher = module.config.matcher;
-
-        const staticPaths = await generator();
-        const result = staticPaths.map((staticPath) => {
-          return matcher.toString(staticPath);
-        });
-
-        return result;
-      }),
-    )
-  ).flat();
-
-  // render rsc and html
-  const baseDir = config.environments.client.build.outDir;
-  for (const staticPatch of staticPaths) {
-    config.logger.info(
-      `\x1b[36m[vite-rsc:ssg]\x1b[0m -> \x1b[32m${staticPatch}\x1b[0m`
-    );
-    const { html, rsc } = await entry.handleSsg(
-      new Request(new URL(staticPatch, "http://ssg.local")),
-    );
-    await writeFileStream(
-      path.join(baseDir, normalize(staticPatch) + HTML_POSTFIX),
-      html,
-    );
-    await writeFileStream(path.join(baseDir, staticPatch + RSC_POSTFIX), rsc);
-  }
-}
-
-async function writeFileStream(filePath: string, stream: ReadableStream) {
-  await fs.promises.mkdir(path.dirname(filePath), { recursive: true });
-  await fs.promises.writeFile(filePath, Readable.fromWeb(stream as never));
-}
-
-export default function rscSSG(): Plugin[] {
+export default function rpress(): Plugin[] {
   return [
     ...rsc({
       entries: {
@@ -87,7 +15,7 @@ export default function rscSSG(): Plugin[] {
       serverHandler: process.env.isPreview ? false : undefined,
       useBuildAppHook: true,
     }),
-    ...rscSsgPlugin(),
+    ...ssg(),
     {
       name: "rsc-ssg",
       configEnvironment(_name, environmentConfig, _env) {
---
+++ packages/rpress/src/vite/ssg.ts
@@ -0,0 +1,77 @@
+import path from "path";
+import { pathToFileURL } from "url";
+import type { Plugin, ResolvedConfig } from "vite";
+import fs from "node:fs";
+import { normalize } from "../utils/path";
+import { HTML_POSTFIX, RSC_POSTFIX } from "../config";
+import { Readable } from "node:stream";
+
+export function rscSsgPlugin(): Plugin[] {
+  return [
+    {
+      name: "rpress:rsc-ssg",
+      config(_config, env) {
+        if (env.isPreview) {
+          return {
+            appType: "mpa",
+          };
+        }
+      },
+      buildApp: {
+        async handler(builder) {
+          await renderStatic(builder.config);
+        },
+      },
+    },
+  ];
+}
+
+
+async function renderStatic(config: ResolvedConfig) {
+  // import server entry
+  const entryPath = path.join(config.environments.rsc.build.outDir, "index.js");
+  const entry: typeof import("../entry/rsc") = await import(
+    pathToFileURL(entryPath).href
+  );
+
+  // entry provides a list of static paths
+  const allRouteModules = entry.allRouteModules;
+
+  const staticPaths = (
+    await Promise.all(
+      allRouteModules.map(async (module) => {
+        const generator = module.config.config.generator;
+        const matcher = module.config.matcher;
+
+        const staticPaths = await generator();
+        const result = staticPaths.map((staticPath) => {
+          return matcher.toString(staticPath);
+        });
+
+        return result;
+      }),
+    )
+  ).flat();
+
+  // render rsc and html
+  const baseDir = config.environments.client.build.outDir;
+  for (const staticPatch of staticPaths) {
+    config.logger.info(
+      `\x1b[36m[vite-rsc:ssg]\x1b[0m -> \x1b[32m${staticPatch}\x1b[0m`
+    );
+    const { html, rsc } = await entry.handleSsg(
+      new Request(new URL(staticPatch, "http://ssg.local")),
+    );
+    await writeFileStream(
+      path.join(baseDir, normalize(staticPatch) + HTML_POSTFIX),
+      html,
+    );
+    await writeFileStream(path.join(baseDir, staticPatch + RSC_POSTFIX), rsc);
+  }
+}
+
+
+async function writeFileStream(filePath: string, stream: ReadableStream) {
+  await fs.promises.mkdir(path.dirname(filePath), { recursive: true });
+  await fs.promises.writeFile(filePath, Readable.fromWeb(stream as never));
+}
