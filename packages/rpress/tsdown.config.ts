3371ecd1ab7b19076715e7397adb3c1b8737ecc3
bloodnighttw <bbeenn1227@gmail.com>
2025-08-29 22:02:28 +0800

    feat: add a helper function.

--- packages/rpress/tsdown.config.ts
+++ packages/rpress/tsdown.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   entry: {
     index: "src/index.ts",
+    helper: "src/core/helper.tsx",
     vite: "src/vite/index.ts",
     "entry/browser": "src/entry/browser.tsx",
     "entry/rsc": "src/entry/rsc.tsx",
