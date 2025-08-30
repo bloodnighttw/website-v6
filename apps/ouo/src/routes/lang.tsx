023df9982710b81d3b3a222539aa3238d89a5946
紅危/bloodnighttw <bbeenn1227@gmail.com>
2025-08-30 16:20:32 +0800

    Revert "Revert "create route type improve""

---
+++ apps/ouo/src/routes/lang.tsx
@@ -0,0 +1,11 @@
+import { createRoute, type RouterProps } from "rpress";
+
+export const route = createRoute("/:lang", {
+  generator: async () => {
+    return [{ lang: "en" }, { lang: "fr" }];
+  },
+});
+
+export default async function LangLayout({ params }: RouterProps<typeof route>) {
+  return <div>Current language: {params.lang}</div>;
+}
