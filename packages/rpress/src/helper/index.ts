38ece0abeb1251c0e3bf1a3d8ead135ac36d29d2
bloodnighttw <bbeenn1227@gmail.com>
2025-08-30 22:01:13 +0800

    feat: change ErrorBoundary import to lazy loading to prevent hydration errors

--- packages/rpress/src/helper/index.ts
+++ packages/rpress/src/helper/index.ts
@@ -1,5 +1,7 @@
+import { lazy } from "react";
 import { FlatComponentHelper } from "./flat-component-helper";
+
 // to prevent hydration errors when import this module
-const ErrorBoundary = await import("./error");
+const ErrorBoundary = lazy(() => import("./error"));
 
 export { FlatComponentHelper, ErrorBoundary };
