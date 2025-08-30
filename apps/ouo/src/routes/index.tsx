f3995a5cbe5e633cafa55f3b14a873520fd266d1
紅危/bloodnighttw <bbeenn1227@gmail.com>
2025-08-30 16:19:41 +0800

    create route type improve (#7)
    * fix: fix when open url then restart vite, trigger code 500
    
    * fix: update route configuration references to use 'route' instead of 'config'
    
    * fix: update createRoute function signature to handle optional config parameter
    
    * feat: add language to test .

--- apps/ouo/src/routes/index.tsx
+++ apps/ouo/src/routes/index.tsx
@@ -3,13 +3,9 @@ import { Counter } from "../counter";
 import RootLayout from "./layouts/root";
 import { FlatComponentHelper } from "rpress/helper";
 
-export const config = createRoute("/", {
-  generator: async () => {
-    return [];
-  },
-});
-
-export default async function WTF(props: RouterProps<typeof config>) {
+export const route = createRoute("/");
+  
+export default async function WTF(props: RouterProps<typeof route>) {
 
   const helper = new FlatComponentHelper();
   helper.expand(RootLayout, {});
