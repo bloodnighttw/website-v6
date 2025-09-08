af88f0acf0548e989ff8fb4f45d7a4326d75ad47
bloodnighttw <bbeenn1227@gmail.com>
2025-09-08 22:36:55 +0800

    refactor: update rsc-loader path and remove unused exports from package.json

--- packages/rpress/src/vite/rsc-loader.ts
+++ packages/rpress/src/vite/rsc-loader.ts
@@ -3,10 +3,16 @@ import { type Plugin } from "vite";
 export default function rscLoader(): Plugin {
   return {
     name: "rpress:rsc-loader",
+    configResolved(config) {
+      config.build.rollupOptions.external = [
+        ...((config.build.rollupOptions.external as string[]) || []),
+        "./node_modules/rpress/dist/rsc-loader",
+      ];
+    },
     resolveId(id) {
       if (id === "virtual:rpress:rsc-loader") {
         if (this.environment.name === "client") {
-          return this.resolve("rpress/rsc-loader");
+          return this.resolve("./node_modules/rpress/dist/rsc-loader");
         }
         return "\0" + id;
       }
