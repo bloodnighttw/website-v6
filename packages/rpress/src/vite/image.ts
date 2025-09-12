492119a624636d78f9563140d6ff19cbc0d1e468
bloodnighttw <bbeenn1227@gmail.com>
2025-09-12 23:58:21 +0800

    refactor: simplify image plugin logic and remove unused code

--- packages/rpress/src/vite/image.ts
+++ packages/rpress/src/vite/image.ts
@@ -1,11 +1,7 @@
 import { type Plugin } from "vite";
-import fs from "node:fs";
-import path from "node:path";
 
 export function image(): Plugin[] {
   let imageOutDir: string;
-  let clientOutDir: string;
-  let isBuild = false;
   return [
     {
       name: "rpress:image",
@@ -16,27 +12,20 @@ export function image(): Plugin[] {
           "./node_modules/rpress/dist/image/server",
         ];
       },
-      buildStart() {
-        isBuild = true;
-      },
       resolveId(id) {
         if (id === "virtual:rpress:image") {
-          if (this.environment.name === "client" || !isBuild) {
+          if (this.environment.name === "client") {
             return this.resolve("./node_modules/rpress/dist/image/loader");
           } else {
             return this.resolve("./node_modules/rpress/dist/image/server");
           }
         }
       },
-      buildEnd() {
-        isBuild = false;
-      },
     },
     {
       name: "rpress:image-base",
       configResolved(config) {
-        imageOutDir = config.build.outDir + "/images/";
-        clientOutDir = config.environments.client.build.outDir;
+        imageOutDir = config.environments.client.build.outDir + "/images/";
       },
       resolveId(id) {
         if (id === "virtual:rpress:image-base") {
@@ -46,30 +35,6 @@ export function image(): Plugin[] {
       load(id) {
         if (id === "\0virtual:rpress:image-base") {
           return `export default "${imageOutDir}"`;
-        }
-      },
-      // copy into client build dir after build
-      async closeBundle() {
-        if (this.environment.name === "ssr") {
-          // copy all files from base to clientOutDir/images
-          const dest = path.join(clientOutDir, "images");
-          if (!fs.existsSync(imageOutDir)) return;
-
-          if (!fs.existsSync(dest)) {
-            await fs.promises.mkdir(dest, { recursive: true });
-          }
-
-          console.log("Copying images to client build:", dest, imageOutDir);
-          // copy all files from imageOutDir to dest
-          const files = await fs.promises.readdir(imageOutDir);
-          await Promise.all(
-            files.map((file) =>
-              fs.promises.copyFile(
-                path.join(imageOutDir, file),
-                path.join(dest, file),
-              ),
-            ),
-          );
         }
       },
     },
