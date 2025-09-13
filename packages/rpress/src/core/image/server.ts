5c74fdf84f85584b668bf3d7085f8b6401db8ef7
bloodnighttw <bbeenn1227@gmail.com>
2025-09-13 23:29:55 +0800

    Add caching to image generation to prevent duplicate requet.

--- packages/rpress/src/core/image/server.ts
+++ packages/rpress/src/core/image/server.ts
@@ -2,7 +2,7 @@ import base from "virtual:rpress:image-base";
 import fs from "fs";
 
 import loader from "./loader";
-import { use } from "react";
+import { cache, use } from "react";
 import type { ImageLoaderOptions } from "./handler";
 import handleImageConversion from "./handler";
 import ShouldThrowError from "../../utils/shouldThrowError";
@@ -14,16 +14,23 @@ class ImageConversionError extends ShouldThrowError {
   }
 }
 
+function checkExist(path: string) {
+  return fs.existsSync(path);
+}
+
+const cacheCheckExist = cache(checkExist);
+
 async function image2file(options: ImageLoaderOptions) {
+  console.log("Generating image for", options.url);
   const { url } = options;
   try {
     const hash = url2Hash(url);
-    if (!url.startsWith("http")) return url; // local file, return as is
-    if (!import.meta.env.PROD) return url; // only generate in production
+    if (!url.startsWith("http")) return; // local file, return as is
+    if (!import.meta.env.PROD) return; // only generate in production
     const path = base + hash + ".webp";
     // if folder not exist, create it
     const dir = base;
-    if (!fs.existsSync(dir)) {
+    if (!cacheCheckExist(path)) {
       await fs.promises.mkdir(dir);
     }
 
@@ -40,9 +47,23 @@ async function image2file(options: ImageLoaderOptions) {
   }
 }
 
+const imageCache = new Map<string, Promise<void>>();
+
+// to avoid multiple requests for the same image
+// this is due to the nature of react's use() hook
+// if we directly use image2file(options) in use(), it will be called multiple times
+// so we cache the promise and return it if it already exists
+function getOrSetCache(options: ImageLoaderOptions) {
+  const hash = url2Hash(options.url, options);
+  if (imageCache.has(hash)) return imageCache.get(hash)!;
+  const promise = image2file(options);
+  imageCache.set(hash, promise);
+  return promise;
+}
+
 export default function handleGeneration(options: ImageLoaderOptions) {
   if (import.meta.env.PROD) {
-    use(image2file(options));
+    use(getOrSetCache(options));
   }
   return loader(options);
 }
