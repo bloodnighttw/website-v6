3e712bfffdf42fb1dfcdd0ea025fc31d72fed9c4
bloodnighttw <bbeenn1227@gmail.com>
2025-09-13 22:49:48 +0800

    fix: Clarify error message in ShouldThrowError class

--- packages/rpress/src/utils/shouldThrowError.tsx
+++ packages/rpress/src/utils/shouldThrowError.tsx
@@ -1,6 +1,6 @@
 // this error will stop rsc to render.
 export default class ShouldThrowError extends Error {
   constructor(type: string, options?: ErrorOptions) {
-    super("A Error that should be caught by framework: " + type, options);
+    super("A Error that shouldn't be caught by framework: " + type, options);
   }
 }
