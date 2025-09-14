0d6b7eada89e0ae3f10f7cc3c6c752e2e4137277
bloodnighttw <bbeenn1227@gmail.com>
2025-09-14 13:51:56 +0800

    feat: update hash generating function to use sha256 to avoid collisions (which possibilty is very very very very low).

--- packages/rpress/src/core/image/server.ts
+++ packages/rpress/src/core/image/server.ts
@@ -1,11 +1,11 @@
 import base from "virtual:rpress:image-base";
 import fs from "fs";
+import * as crypto from "crypto";
 
 import { cache, use } from "react";
 import type { ImageLoaderOptions } from "./handler";
 import handleImageConversion from "./handler";
 import ShouldThrowError from "../../utils/shouldThrowError";
-import { url2Hash } from "./url2Hash";
 import path from "path";
 
 class ImageConversionError extends ShouldThrowError {
@@ -14,36 +14,36 @@ class ImageConversionError extends ShouldThrowError {
   }
 }
 
-function serverLoader(options: ImageLoaderOptions) {
-  const { url, quality, width, height } = options;
-  if (!url.startsWith("http")) return url; // local file, return as is
-
-  if (import.meta.env.DEV) {
-    return (
-      "_rpress?url=" +
-      encodeURIComponent(url) +
-      (quality ? `&quality=${quality}` : "") +
-      (width ? `&width=${width} ` : "") +
-      (height ? `&height=${height}` : "")
-    );
-  }
-
-  return "/images/" + url2Hash(url, options) + ".webp";
-}
-
 function checkExist(path: string) {
   return fs.existsSync(path);
 }
 
 const cacheCheckExist = cache(checkExist);
 
+function generateSHA256(obj: ImageLoaderOptions) {
+  // Recursively sort all object keys for deterministic serialization
+  const sortedObj: ImageLoaderOptions = {
+    url: obj.url,
+    width: obj.width,
+    height: obj.height,
+    quality: obj.quality,
+  };
+
+  const jsonString = JSON.stringify(sortedObj);
+
+  return crypto.createHash("sha256").update(jsonString, "utf8").digest("hex");
+}
+
 async function image2file(options: ImageLoaderOptions) {
   console.log("Generating image for", options.url);
   const { url } = options;
+  const hash = generateSHA256(options);
   try {
-    const hash = url2Hash(url);
-    if (!url.startsWith("http")) return; // local file, return as is
-    if (!import.meta.env.PROD) return; // only generate in production
+    if (!url.startsWith("http")) return url; // local file, return as is
+    if (!import.meta.env.PROD)
+      throw new ShouldThrowError(
+        "file Generating should only bundle in production!",
+      ); // only generate in production
     const filePath = path.join(base, hash + ".webp");
     // if folder not exist, create it
     const dir = base;
@@ -58,19 +58,21 @@ async function image2file(options: ImageLoaderOptions) {
     const arrayBuffer = await fetchFromurl.arrayBuffer();
     const convertedBuffer = await handleImageConversion(arrayBuffer, options);
     await fs.promises.writeFile(filePath, convertedBuffer);
+
+    return "/images/" + hash + ".webp";
   } catch (e) {
     throw new ImageConversionError(url, { cause: e });
   }
 }
 
-const imageCache = new Map<string, Promise<void>>();
+const imageCache = new Map<string, Promise<string>>();
 
 // to avoid multiple requests for the same image
 // this is due to the nature of react's use() hook
 // if we directly use image2file(options) in use(), it will be called multiple times
 // so we cache the promise and return it if it already exists
 function getOrSetCache(options: ImageLoaderOptions) {
-  const hash = url2Hash(options.url, options);
+  const hash = generateSHA256(options);
   if (imageCache.has(hash)) return imageCache.get(hash)!;
   const promise = image2file(options);
   imageCache.set(hash, promise);
@@ -78,8 +80,14 @@ function getOrSetCache(options: ImageLoaderOptions) {
 }
 
 export default function handleGeneration(options: ImageLoaderOptions) {
-  if (import.meta.env.PROD) {
-    use(getOrSetCache(options));
+  if (import.meta.env.DEV) {
+    return (
+      "_rpress?url=" +
+      encodeURIComponent(options.url) +
+      (options.quality ? `&quality=${options.quality}` : "") +
+      (options.width ? `&width=${options.width} ` : "") +
+      (options.height ? `&height=${options.height}` : "")
+    );
   }
-  return serverLoader(options);
+  return use(getOrSetCache(options));
 }
