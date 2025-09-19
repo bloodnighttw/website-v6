2055a82598661f45dceb38fd5356c663a2d6f85f
bloodnighttw <bbeenn1227@gmail.com>
2025-09-19 21:46:30 +0800

    Remove unused virtual config plugin from RPress Vite config

--- packages/rpress/src/vite/config.ts
+++ packages/rpress/src/vite/config.ts
@@ -1,30 +1,11 @@
 import { type Plugin } from "vite";
 import { type RPressConfig } from "@/vite/type";
 
-const VIRTUAL_RPRESS_CONFIG = "virtual:rpress:config";
 const VIRTUAL_RPRESS_ROUTES = "virtual:rpress:routes";
-const RESOLVED_VIRTUAL_RPRESS_CONFIG = "\0" + VIRTUAL_RPRESS_CONFIG;
 const RESOLVED_VIRTUAL_RPRESS_ROUTES = "\0" + VIRTUAL_RPRESS_ROUTES;
 
 export default function RPressConfig(config: RPressConfig): Plugin[] {
   return [
-    {
-      name: "rpress:config",
-      resolveId(id) {
-        if (id === VIRTUAL_RPRESS_CONFIG) {
-          return RESOLVED_VIRTUAL_RPRESS_CONFIG;
-        }
-      },
-      load(id) {
-        if (id === RESOLVED_VIRTUAL_RPRESS_CONFIG) {
-          return {
-            code: `export default ${JSON.stringify(config)};`,
-            moduleType: "js",
-          };
-        }
-      },
-    },
-
     {
       name: "rpress:inject-route",
       resolveId(id) {
