1749cd8517978c0e8139a599d024c7b0d5eccf66
bloodnighttw <bbeenn1227@gmail.com>
2025-09-27 13:35:43 +0800

    chore: Move global styles to src and update imports and colors

--- apps/ouo/src/routes/index.tsx
+++ apps/ouo/src/routes/index.tsx
@@ -14,7 +14,7 @@ export default async function WTF(props: RouterProps<typeof route>) {
 
   return (
     <Flatten>
-      <div className="dark:bg-amber-200">hi from home</div>
+      <div>hi from home</div>
       <div>props</div>
       <div>data: {JSON.stringify(props.params)}</div>
       <Counter />
--- apps/ouo/src/routes/layouts/root.tsx
+++ apps/ouo/src/routes/layouts/root.tsx
@@ -1,3 +1,4 @@
+import "@/global.css";
 import "./root.css";
 
 interface RootProps {
