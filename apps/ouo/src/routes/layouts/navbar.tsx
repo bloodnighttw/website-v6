6b6d1f8a73f220ba6d0c580993ec07125299f2d2
bloodnighttw <bbeenn1227@gmail.com>
2025-10-02 20:26:42 +0800

    feat: make image don't resize.

--- apps/ouo/src/routes/layouts/navbar.tsx
+++ apps/ouo/src/routes/layouts/navbar.tsx
@@ -8,12 +8,7 @@ export default function Navbar() {
   return (
     <nav className="container sticky top-0 w-screen xl:w-full xl:top-2 xl:mt-2 bg-primary-500/10 xl:rounded-xl backdrop-blur-2xl">
       <div className="flex items-center h-16 gap-2 ">
-        <Image
-          src={setting.avatar}
-          width={32}
-          height={32}
-          className="rounded-full"
-        />
+        <Image src={setting.avatar} className="rounded-full size-8" />
         <Link to="/" className="text-lg font-bold">
           Bloodnighttw's Workshop
         </Link>
