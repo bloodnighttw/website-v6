a0f1bba70435eadaf0218b4fe167675f107ebbad
bloodnighttw <bbeenn1227@gmail.com>
2025-10-05 17:10:59 +0800

    Refine transition classes and add global color transitions

--- apps/ouo/src/components/card/label.tsx
+++ apps/ouo/src/components/card/label.tsx
@@ -7,7 +7,7 @@ export default function CardLabel(props: { children: React.ReactNode }) {
       className={cn(
         "h-10 py-2 px-8 flex justify-center items-center",
         "rounded-full text-lg hover:bg-secondary-950/5 dark:hover:bg-secondary-100/5",
-        "duration-200 transition-none hover:transition-colors",
+        "duration-200 transition-colors",
       )}
     >
       {props.children}
