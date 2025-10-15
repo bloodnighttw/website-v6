f7fd30fc077885998ac7a51b4104f25962bba2af
bloodnighttw <bbeenn1227@gmail.com>
2025-10-15 21:13:23 +0800

    chore: Add i18n context and translation files for en and zh

--- apps/ouo/src/contexts/i18n.tsx
+++ apps/ouo/src/contexts/i18n.tsx
@@ -1,14 +1,25 @@
 "use client";
 
-import { createContext, type ReactNode } from "react";
+import { createContext, use, type ReactNode } from "react";
 
 const lang = ["en", "zh"] as const;
 
-const I18nContext = createContext<(typeof lang)[number]>("en");
-
 export type Lang = (typeof lang)[number];
+type Translate = typeof import("@/config/i18n/zh.json");
+
+interface I18nContextType {
+  lang: Lang;
+  translate: Translate;
+}
+
+const I18nContext = createContext<I18nContextType | null>(null);
 
 export { I18nContext };
+
+const translations: Record<Lang, () => Promise<Translate>> = {
+  zh: () => import("@/config/i18n/zh.json"),
+  en: () => import("@/config/i18n/en.json"),
+};
 
 export default function I18nProvider({
   children,
@@ -17,5 +28,10 @@ export default function I18nProvider({
   children: ReactNode;
   lang: Lang;
 }) {
-  return <I18nContext.Provider value={lang}>{children}</I18nContext.Provider>;
+  const translate = use(translations[lang]());
+  return (
+    <I18nContext.Provider value={{ lang, translate }}>
+      {children}
+    </I18nContext.Provider>
+  );
 }
