b4127db4727e379dd4767f9a082f1ef438a60489
bloodnighttw <bbeenn1227@gmail.com>
2025-10-18 19:10:26 +0800

    refactor: use globalThis instead calling fs.write inside ssg.

--- packages/rpress/src/libs/image/client.ts
+++ packages/rpress/src/libs/image/client.ts
@@ -1,6 +1,12 @@
+import IS_CLIENT from "virtual:rpress:client-env";
 import type { ImageLoaderOptions } from "./handler";
 
 export default function loader(options: ImageLoaderOptions) {
+  console.log("client image loader called");
+  // it will be treeshaken in client build.
+  if (!IS_CLIENT) {
+    globalThis.__RPRESS_IMAGES__?.push(options);
+  }
   console.warn("image convert cannot be done inside non-ssr component");
   return options.url;
 }
--- packages/rpress/src/vite/image.ts
+++ packages/rpress/src/vite/image.ts
@@ -1,45 +1,42 @@
-import path from "path";
+import type { ImageLoaderOptions } from "@/libs/image/handler";
 import { type Plugin } from "vite";
 
+const IMAGE_NEED_PROCESSING_VID = "virtual:rpress:image:mode";
+
+declare global {
+  var __RPRESS_IMAGES__: ImageLoaderOptions[] | undefined;
+}
+
 export function image(): Plugin[] {
-  let imageOutDir: string;
+  globalThis.__RPRESS_IMAGES__ = [];
+  let NEED_GENERATE = false;
+
   return [
     {
       name: "rpress:image",
-      configResolved(config) {
-        config.build.rollupOptions.external = [
-          // ...((config.build.rollupOptions.external as string[]) || []),
-          "./node_modules/rpress/dist/image/client",
-          "./node_modules/rpress/dist/image/server",
-        ];
+      configureServer() {
+        // we don't need it when server is running
+        globalThis.__RPRESS_IMAGES__ = undefined;
+        NEED_GENERATE = false;
       },
       resolveId(id) {
-        if (id === "virtual:rpress:image") {
-          if (this.environment.name === "client") {
-            return this.resolve("./node_modules/rpress/dist/image/client");
-          } else {
-            return this.resolve("./node_modules/rpress/dist/image/server");
-          }
-        }
-      },
-    },
-    {
-      name: "rpress:image-base",
-      configResolved(config) {
-        imageOutDir = path.join(
-          config.environments.client.build.outDir,
-          "images",
-        );
-      },
-      resolveId(id) {
-        if (id === "virtual:rpress:image-base") {
-          return "\0" + id;
+        if (id === IMAGE_NEED_PROCESSING_VID) {
+          return "\0" + IMAGE_NEED_PROCESSING_VID;
         }
       },
       load(id) {
-        if (id === "\0virtual:rpress:image-base") {
-          return `export default "${imageOutDir}"`;
+        if (id === "\0" + IMAGE_NEED_PROCESSING_VID) {
+          return `export default ${NEED_GENERATE ? JSON.stringify("generation") : JSON.stringify("dynamic")};`;
         }
+      },
+      buildApp: {
+        order: "post",
+        async handler() {
+          console.log("Images to process:!!!!!!!!");
+          globalThis.__RPRESS_IMAGES__?.forEach((option) => {
+            console.log(option);
+          });
+        },
       },
     },
   ];
