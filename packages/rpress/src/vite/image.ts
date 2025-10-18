a8c7a18bfae6d46827f602974b3ce23187bc1747
bloodnighttw <bbeenn1227@gmail.com>
2025-10-18 23:25:40 +0800

    chore: Process and write converted images during build.

--- packages/rpress/src/vite/image.ts
+++ packages/rpress/src/vite/image.ts
@@ -1,5 +1,9 @@
 import type { ImageLoaderOptions } from "@/libs/image/handler";
+import handleImageConversion from "@/libs/image/handler";
+import { generateSHA256 } from "@/libs/image/sha256";
+import path from "path";
 import { type Plugin } from "vite";
+import fs from "fs";
 
 const IMAGE_NEED_PROCESSING_VID = "virtual:rpress:image:mode";
 
@@ -9,11 +13,15 @@ declare global {
 
 export function image(): Plugin[] {
   globalThis.__RPRESS_IMAGES__ = [];
-  let NEED_GENERATE = false;
+  let NEED_GENERATE = true;
+  let outDir = "";
 
   return [
     {
       name: "rpress:image",
+      configResolved(config) {
+        outDir = path.join(config.environments.client.build.outDir, "_rpress");
+      },
       configureServer() {
         // we don't need it when server is running
         globalThis.__RPRESS_IMAGES__ = undefined;
@@ -32,10 +40,38 @@ export function image(): Plugin[] {
       buildApp: {
         order: "post",
         async handler() {
-          console.log("Images to process:!!!!!!!!");
-          globalThis.__RPRESS_IMAGES__?.forEach((option) => {
-            console.log(option);
-          });
+          console.log("[rpress:image] Processing images...");
+          if (
+            !globalThis.__RPRESS_IMAGES__ ||
+            globalThis.__RPRESS_IMAGES__.length == 0
+          ) {
+            return;
+          }
+
+          for (const imgOptions of globalThis.__RPRESS_IMAGES__) {
+            console.log("[Rpress-Image]", imgOptions.url);
+            const sha256 = generateSHA256(imgOptions);
+            const imageRequest = await fetch(imgOptions.url);
+            if (!imageRequest.ok) {
+              throw new Error(`Failed to fetch image: ${imgOptions.url}`);
+            }
+            const buffer = await imageRequest.arrayBuffer();
+
+            const processedBuffer = await handleImageConversion(
+              buffer,
+              imgOptions,
+            );
+
+            const outputFilePath = path.join(outDir, `${sha256}.webp`);
+
+            await fs.promises.mkdir(path.dirname(outputFilePath), {
+              recursive: true,
+            });
+            await fs.promises.writeFile(
+              outputFilePath,
+              Buffer.from(processedBuffer),
+            );
+          }
         },
       },
     },
