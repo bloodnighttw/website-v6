69cf9bf04ce591d43ef61612cd42167cc1f0b96e
bloodnighttw <bbeenn1227@gmail.com>
2025-10-21 00:47:17 +0800

    feat: update navbar style.

--- apps/ouo/src/layouts/navbar/index.tsx
+++ apps/ouo/src/layouts/navbar/index.tsx
@@ -5,16 +5,27 @@ import "server-only";
 import ThemeButton from "./theme-button";
 import LangButton from "./lang-button";
 import type { Lang } from "@/utils/i18n/config";
+import { cn } from "@/utils/cn";
+import CardLabel from "@/components/card/label";
+import { IoPeople } from "react-icons/io5";
 
 export default function Navbar({ lang }: { lang: Lang }) {
   return (
-    <nav className="container sticky top-0 w-screen xl:w-full xl:top-2 xl:mt-2 xl:card bg-primary-500/10 xl:rounded-xl backdrop-blur-2xl">
-      <div className="flex items-center min-h-16 gap-2">
+    <nav className={cn("container *:px-4 md:*:px-6")}>
+      <div
+        className={cn(
+          "flex items-center min-h-16 gap-4 card mt-2 bg-primary-500/10 rounded-full backdrop-blur-2xl",
+        )}
+      >
         <Link to={`/${lang}`} className="text-lg font-bold">
           <Image src={setting.avatar} className="rounded-full size-8" />
         </Link>
-        <div className="mx-auto" />
-        <LangButton />
+        <CardLabel className="px-0 h-7 w-12 mx-auto md:mx-0">
+          <IoPeople size={16} className="" />
+        </CardLabel>
+        <div className="mx-auto md:mx-0 md:ml-auto flex">
+          <LangButton />
+        </div>
         <ThemeButton />
       </div>
     </nav>
