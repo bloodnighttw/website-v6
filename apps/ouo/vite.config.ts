3ad06ba1a83240e9426f179ab2587492b81ab9ec
bloodnighttw <bbeenn1227@gmail.com>
2026-01-31 01:36:51 +0800

    Update vite.config.ts

--- apps/ouo/vite.config.ts
+++ apps/ouo/vite.config.ts
@@ -65,7 +65,7 @@ export default defineConfig({
     }),
     mdx([pj, blog]),
     stylex.vite({
-      // useCSSLayers: true,
+      useCSSLayers: true,
       debug: true,
       aliases: {
         "@/*": [
